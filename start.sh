e981aff7660cdb433df232360140740fb0ef998d
john <javadkhodaverdi221@gmail.com>
2025-04-15 19:02:34 +0330

    get ready for render

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# نصب کروم
+apt-get update
+apt-get install -y wget gnupg unzip
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+apt-get update
+apt-get install -y google-chrome-stable
+
+# اجرای برنامه
+python main.py
