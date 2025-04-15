8a21aa5a8f69763a5838e67588db17e1633c22c2
john <javadkhodaverdi221@gmail.com>
2025-04-15 19:41:23 +0330

    test

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import json
 import re
